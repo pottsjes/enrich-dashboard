6be23c2362f5196670059d8ce901ea1ea060f337
Jesse Potts <pottsjes@gmail.com>
2025-05-23 17:19:53 -0700

    honing in report design, optimized creation

--- constants/constants.py
+++ constants/constants.py
@@ -1,21 +1,32 @@
+# Data Columns
 KEY_LISTING_NAME = "Listing Name"
 KEY_REVPAR_INDEX = "RevPAR Index"
 KEY_REVPAR_INDEX_STLY = "RevPAR Index STLY"
-KEY_REVPAR_STLY = "RevPAR STLY"
+KEY_TOTAL_REVPAR = "Total RevPAR"
+KEY_TOTAL_REVPAR_STLY = "Total RevPAR STLY"
+KEY_MARKET_REVPAR = "Market RevPAR"
 KEY_MARKET_REVPAR_STLY = "Market RevPAR STLY"
 KEY_MARKET_PEN = "Market Penetration Index %"
 KEY_MARKET_PEN_STLY = "Market Penetration Index STLY"
+KEY_PAID_OCCUPANCY = "Paid Occupancy %"
 KEY_PAID_OCCUPANCY_STLY = "Paid Occupancy % STLY"
+KEY_MARKET_OCCUPANCY = "Market Occupancy %"
 KEY_MARKET_OCCUPANCY_STLY = "Market Occupancy % STLY"
-KEY_REVPAR_STLY_YOY = "RevPAR STLY YoY %"
-KEY_TOTAL_REV_YOY = "Total Revenue STLY YoY %"
-KEY_OCC_STLY = "Occupancy STLY YoY Difference"
-KEY_REVPAR_PICKUP = "RevPAR Pickup"
+KEY_TOTAL_ADR = "Total ADR"
+KEY_TOTAL_ADR_STLY = "Total ADR STLY"
 KEY_ADR_INDEX = "ADR Index"
-KEY_ADR_STLY_YOY = "ADR STLY YoY %"
 KEY_ADR_INDEX_STLY = "ADR Index STLY"
-KEY_ADR_STLY = "ADR STLY"
+KEY_MARKET_ADR = "Market ADR"
 KEY_MARKET_ADR_STLY = "Market ADR STLY"
+KEY_BOOKED_NIGHTS_PICKUP = "Booked Nights Pickup (30 Days)"
+KEY_OCCUPANCY = "Occupancy %"
+KEY_OCCUPANCY_STLY = "Occupancy % STLY"
+KEY_TOTAL_REVENUE = "Total Revenue"
+KEY_TOTAL_REVENUE_STLY = "Total Revenue STLY"
+
+# App Constants
 KEY_LABELS = "Labels"
+REPORT_WIDTH = 297
+REPORT_HEIGHT = 210
 
 customers = ["Pierce Gainey", "Austin Whitaker"]
--- helpers/upload.py
+++ helpers/upload.py
@@ -1,12 +1,8 @@
+import base64
 from io import BytesIO
-import re
-import tempfile
-from openpyxl import load_workbook
 import streamlit as st
 import pandas as pd
 import os
-import base64
-from jinja2 import Template
 import plotly.graph_objects as go
 import plotly.io as pio
 from fpdf import FPDF
@@ -15,16 +11,29 @@ from constants.constants import (
     KEY_LISTING_NAME,
     KEY_REVPAR_INDEX,
     KEY_REVPAR_INDEX_STLY,
-    KEY_REVPAR_STLY,
+    KEY_TOTAL_REVPAR,
+    KEY_TOTAL_REVPAR_STLY,
+    KEY_MARKET_REVPAR,
     KEY_MARKET_REVPAR_STLY,
     KEY_MARKET_PEN,
     KEY_MARKET_PEN_STLY,
+    KEY_PAID_OCCUPANCY,
     KEY_PAID_OCCUPANCY_STLY,
+    KEY_OCCUPANCY,
+    KEY_OCCUPANCY_STLY,
+    KEY_MARKET_OCCUPANCY,
     KEY_MARKET_OCCUPANCY_STLY,
     KEY_ADR_INDEX_STLY,
-    KEY_ADR_STLY,
+    KEY_TOTAL_ADR,
+    KEY_TOTAL_ADR_STLY,
+    KEY_MARKET_ADR,
     KEY_MARKET_ADR_STLY,
+    KEY_TOTAL_REVENUE,
+    KEY_TOTAL_REVENUE_STLY,
+    KEY_BOOKED_NIGHTS_PICKUP,
     KEY_LABELS,
+    REPORT_HEIGHT,
+    REPORT_WIDTH,
     customers
 )
 
@@ -53,7 +62,12 @@ def get_diff_percent_bar(df: pd.DataFrame, x: str, y: str, title: str, yaxis_tit
             zeroline=False,
             showgrid=True
         ),
+        xaxis=dict(
+            tickangle=45,
+            showticklabels=True
+        ),
         title=title,
+        title_font_size=30,
         shapes=[
             dict(
                 type="line",
@@ -66,28 +80,111 @@ def get_diff_percent_bar(df: pd.DataFrame, x: str, y: str, title: str, yaxis_tit
         ],
         plot_bgcolor="rgba(0,0,0,0)",
         paper_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="white")
+        font=dict(color="black")
     )
     return fig
 
+def listing_metric_table(df, metric_current, metric_stly, title, base=1.0):
+    #sort values in reverse
+    df = df.sort_values(by=[metric_current], ignore_index=True, ascending=False)
+    df = df[(df[metric_current] != 0.0) | (df[metric_stly] != 0.0)].reset_index(drop=True)
+    font_size = 20 * min(20/ len(df), 1)
+    
+    fig = go.Figure()
+    for i, row in df.iterrows():
+        y = -i * 2
+        name = row["Listing Name"]
+        curr_val = row[metric_current]
+        stly_val = row[metric_stly]
+
+        # Listing name on left
+        fig.add_shape(
+            type="rect",
+            x0=0,
+            x1=7,
+            y0=y - 1,
+            y1=y + 1,
+            fillcolor="rgba(100,155,200,0.15)" if (i%2) == 0 else "white",
+            line=dict(width=0),
+            layer="below",
+        )
+        fig.add_trace(go.Scatter(
+            x=[3],
+            y=[y],
+            text=[name],
+            mode="text",
+            textfont=dict(size=font_size, color="black"),
+            textposition="middle left",
+            showlegend=False
+        ))
+
+        def add_bar(value, y_offset, period_label):
+            bar_len = value - base
+            value_offset = (bar_len / abs(bar_len)) * 0.1 if bar_len != 0 else 0
+            color = "lightgray" if bar_len == -1 else "lightgreen" if bar_len > 0 else "pink"
+            x = 5
+            # Bar shape
+            fig.add_shape(
+                type="rect",
+                x0=x,
+                x1=x + bar_len,
+                y0=y + y_offset - 0.3,
+                y1=y + y_offset + 0.3,
+                fillcolor=color,
+                line=dict(width=0),
+                layer="below",
+            )
+            # Bar value label
+            fig.add_trace(go.Scatter(
+                x=[x + (bar_len + value_offset)],
+                y=[y + y_offset],
+                text=[f"{value:.2f}" if value != 0.0 else ""],
+                mode="text",
+                textfont=dict(size=font_size),
+                showlegend=False
+            ))
+            # Period label
+            fig.add_trace(go.Scatter(
+                x=[x-1.8],
+                y=[y + y_offset],
+                text=[period_label],
+                mode="text",
+                textfont=dict(size=font_size),
+                textposition="middle right",
+                showlegend=False
+            ))
+
+        add_bar(curr_val, 0.3, "Current")     # Current metric
+        add_bar(stly_val, -0.3, "STLY")   # STLY metric
+
+    fig.update_layout(
+        title=dict(text=title, x=0.5),
+        title_font_size=50,
+        xaxis=dict(showgrid=False, showticklabels=False, zeroline=False, range=[0, 7]),
+        yaxis=dict(showgrid=False, showticklabels=False, zeroline=False, range=[-len(df)*2 + 1, 1]),
+        height=1600,
+        width=2400
+    )
+
+    return fig
+
+
 def charts_for_listing(row):
-    def make_comparison_chart(title, current_val, stly_val):
+    def make_comparison_chart(title, left_key, left_val, right_key, right_val, percent=False):
         fig = go.Figure()
         fig.add_trace(go.Bar(
-            x=["Current"],
-            y=[current_val],
-            name="Current",
+            y=[left_val],
+            name=left_key,
             marker_color="lightgray",
-            text=[f"{current_val:.0%}"],
+            text=[f"{left_val:.0f}%" if percent else f"${left_val:.0f}"],
             textposition="auto",
             textfont=dict(size=20)
         ))
         fig.add_trace(go.Bar(
-            x=["STLY"],
-            y=[stly_val],
-            name="STLY",
+            y=[right_val],
+            name=right_key,
             marker_color="black",
-            text=[f"{stly_val:.0%}"],
+            text=[f"{right_val:.0f}%" if percent else f"${right_val:.0f}"],
             textposition="auto",
             textfont=dict(size=20)
         ))
@@ -108,13 +205,16 @@ def charts_for_listing(row):
                 x=1
             ),
             font=dict(color="black"),
+            title_font_size=30,
             margin=dict(t=50)
         )
         return fig
 
     return [
-        make_comparison_chart("RevPAR Index", row[KEY_REVPAR_INDEX], row[KEY_REVPAR_INDEX_STLY]),
-        make_comparison_chart("Market Penetration Index", row[KEY_MARKET_PEN], row[KEY_MARKET_PEN_STLY]),
+        make_comparison_chart("Total Occupancy", "Current", row[KEY_OCCUPANCY], "STLY", row[KEY_OCCUPANCY_STLY], percent=True),
+        make_comparison_chart("Marlet Occupancy", "Current", row[KEY_MARKET_OCCUPANCY], "STLY", row[KEY_MARKET_OCCUPANCY_STLY], percent=True), 
+        make_comparison_chart("Total Revenue", "Current", row[KEY_TOTAL_REVENUE], "STLY", row[KEY_TOTAL_REVENUE_STLY]),
+        make_comparison_chart("RevPAR", "Current", row[KEY_TOTAL_REVPAR], "Market", row[KEY_MARKET_REVPAR])
     ]
 
 
@@ -152,10 +252,11 @@ def render_upload_page():
             type=["csv", "xlsx"], 
             help="Upload a CSV or Excel file."
         )
+        uploaded_file = "data/new_data.xlsx"  # For testing purposes
         submit_button = st.form_submit_button(label="Generate Report", help="Click to generate the report.")
 
     # Ensure both fields are filled before proceeding
-    if submit_button:
+    if uploaded_file:
         if not selected_customer:
             st.error("Please select a customer.")
         elif not uploaded_file:
@@ -168,16 +269,15 @@ def render_upload_page():
                 df = pd.read_excel(uploaded_file)
 
             if df is not None:
-                df = df.fillna('')
                 logo_paths = glob.glob(os.path.join("customer_logos", selected_customer + ".*"))
                 logo_path = logo_paths[0]
 
                 # Calculations
                 df[KEY_REVPAR_INDEX] = df[KEY_REVPAR_INDEX] / 100
                 df[KEY_MARKET_PEN] = df[KEY_MARKET_PEN] / 100
-                df[KEY_REVPAR_INDEX_STLY] = df[KEY_REVPAR_STLY] / df[KEY_MARKET_REVPAR_STLY]
+                df[KEY_REVPAR_INDEX_STLY] = df[KEY_TOTAL_REVPAR_STLY] / df[KEY_MARKET_REVPAR_STLY]
                 df[KEY_MARKET_PEN_STLY] = df[KEY_PAID_OCCUPANCY_STLY] / df[KEY_MARKET_OCCUPANCY_STLY]
-                df[KEY_ADR_INDEX_STLY] = df[KEY_ADR_STLY] / df[KEY_MARKET_ADR_STLY]
+                df[KEY_ADR_INDEX_STLY] = df[KEY_TOTAL_ADR_STLY] / df[KEY_MARKET_ADR_STLY]
 
                 # Create Charts
                 df[KEY_LABELS] = df[KEY_LISTING_NAME].str[:20] + "..."
@@ -186,95 +286,103 @@ def render_upload_page():
                 mpi_thisPeriod = get_diff_percent_bar(df, KEY_LABELS, KEY_MARKET_PEN, "Market Penetration Index", "MPI", 1)
                 mpi_stly = get_diff_percent_bar(df, KEY_LABELS, KEY_MARKET_PEN_STLY, "Market Penetration Index STLY", "MPI", 1)
     
-                def get_imgs(charts):
-                    img_bytes = []
-                    for chart in charts:
-                        buffer = BytesIO(pio.to_image(chart, format='png'))
-                        buffer.name = f"{chart['layout']['title']['text']}.png".replace(" ", "_")
-                        buffer.seek(0)
-                        img_bytes.append(buffer)
-                    return img_bytes
+                def add_img(pdf, chart, x, y, w = None, h = None):
+                    buffer = BytesIO(pio.to_image(chart, format='png'))
+                    buffer.name = f"{chart['layout']['title']['text']}.png".replace(" ", "_")
+                    buffer.seek(0)
+                    if w:
+                        pdf.image(buffer, x=x, y=y, w=w)
+                    elif h:
+                        pdf.image(buffer, x=x, y=y, h=h)
 
                 charts = [rpi_thisPeriod, rpi_stly, mpi_thisPeriod, mpi_stly]
-                imgs = get_imgs(charts)
                 # Create a PDF document
-                pdf = FPDF()
+                pdf = FPDF(orientation="L")
                 pdf.set_auto_page_break(auto=True, margin=15)
-                pdf.add_page()
-                pdf.set_font("Arial", size=12)
-                pdf.image(logo_path, x=10, y=8, w=33)
-                pdf.cell(200, 10, txt=f"Monthly Revenue Report - {selected_customer} ", ln=True, align="C")
-                pdf.ln(20)
-                temp_paths = []
-                for i, chart in enumerate(charts):
-                    chart.update_layout(
-                        font=dict(color="black")
-                    )
-                    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
-                        chart.write_image(tmp, format="png")
-                        temp_paths.append(tmp.name)
-                        pdf.image(tmp.name, x=10 + ((i%2)*90), y=pdf.get_y(), w=80)
-                        if i % 2 == 1:
-                            pdf.ln(90)
+                # Create header page
+                def create_table_page(current_metric, stly_metric, title):
+                    pdf.add_page()
+                    pdf.set_fill_color(200, 190, 180)  # muted beige background
+                    pdf.rect(0, 0, REPORT_WIDTH, 35, style='F')
+                    pdf.set_font("Arial", size=30)
+                    pdf.set_xy(15, 8)
+                    pdf.cell(267, 20, txt=f"Monthly Revenue Report - {selected_customer} ", ln=True, align="C")
+                    pdf.image(logo_path, x=15, y=5, w=25)
+                    table = listing_metric_table(df, current_metric, stly_metric, title)
+                    add_img(pdf, table, x=25, y=40, h=165)
+
+                # Testing
+                create_table_page(KEY_REVPAR_INDEX, KEY_REVPAR_INDEX_STLY, "RevPAR Index")
+                create_table_page(KEY_MARKET_PEN, KEY_MARKET_PEN_STLY, "Market Penetration Index")
                 
+                # Create individual listing pages
                 for i, row in df.iterrows():
+                    # if i > 3:
+                    #     break
                     listing_name = row[KEY_LISTING_NAME]
                     pdf.add_page()
                     # Header
                     pdf.set_fill_color(200, 190, 180)  # muted beige background
-                    pdf.rect(0, 0, 210, 35, style='F')
-                    pdf.set_xy(10, 8)
-                    pdf.set_font("Arial", "B", 20)
+                    pdf.rect(0, 0, REPORT_WIDTH, 35, style='F')
+                    pdf.set_xy(15, 8)
+                    title_font_size = 20 * min(1, (65/len(listing_name)))
+                    pdf.set_font("Arial", "B", title_font_size)
                     pdf.set_text_color(0, 0, 0)
-                    pdf.cell(0, 10, listing_name, ln=True)
-                    pdf.set_xy(10, 18)
+                    pdf.cell(240, 10, listing_name, ln=True)
+                    pdf.set_xy(15, 18)
                     pdf.set_font("Arial", "", 12)
-                    print(f"{15 - i}")
-                    pdf.cell(0, 10, f"{month} Report - {row['Group Name']}", ln=False)
+                    print(f"{i + 1}")
+                    pdf.cell(0, 10, f"{month} {year} Report", ln=False)
                     # Logo
-                    pdf.image(logo_path, x=175, y=5, w=25)
+                    pdf.image(logo_path, x=257, y=5, w=25)
                     # KPI Boxes
                     pdf.set_font("Arial", "B", 12)
                     pdf.set_text_color(0, 0, 0)
-                    kpi_box_start_x = 10
                     box_width = 60
-                    box_margin = 5
-                    kpi_box_y = 45
-                    def draw_kpi(label, value, x, label_font_size):
-                        pdf.set_xy(x, kpi_box_y)
+                    box_height = 15
+                    box_margin = 15
+                    kpi_box_start_y = 57
+                    kpi_box_x = 30
+                    def draw_kpi(label, value, y, label_font_size):
+                        pdf.set_xy(kpi_box_x, y)
                         pdf.set_font("Arial", "", label_font_size)
-                        pdf.cell(box_width, 10, txt=label, ln=True, border=1, align="C")
-                        pdf.set_xy(x, kpi_box_y + 10)
+                        pdf.cell(box_width, box_height, txt=label, ln=True, border=1, align="C")
+                        pdf.set_xy(kpi_box_x, y + box_height)
                         pdf.set_font("Arial", "", 16)
-                        pdf.cell(box_width, 10, txt=str(value), ln=True, border=1, align="C")
+                        if not value or value == "0%":
+                            value = ''
+                        pdf.cell(box_width, box_height, txt=str(value), ln=True, border=1, align="C")
 
-                    draw_kpi("Market Penetration Index", f"{int(row[KEY_MARKET_PEN])}", kpi_box_start_x, 14)
-                    # draw_kpi("Booked Nights Pickup\n(Past 30 Days)", int(row["Booked Nights Pickup (30 Days)"]), kpi_box_start_y + box_height + box_margin)
-                    draw_kpi("Booked Nights Pickup\n(Past 30 Days)", 10, kpi_box_start_x + box_width + box_margin, 9)
-                    draw_kpi("RevPAR Index", f"{int(row[KEY_REVPAR_INDEX])}%", kpi_box_start_x + 2*(box_width + box_margin), 15)
+                    draw_kpi(KEY_MARKET_PEN, f"{row[KEY_MARKET_PEN]:.0%}", kpi_box_start_y, 13)
+                    draw_kpi(KEY_BOOKED_NIGHTS_PICKUP, row[KEY_BOOKED_NIGHTS_PICKUP], kpi_box_start_y + (box_height*2) + box_margin, 11)
+                    draw_kpi(KEY_REVPAR_INDEX, f"{row[KEY_REVPAR_INDEX]:.0%}", kpi_box_start_y + 2*((box_height*2) + box_margin), 15)
                     # Charts
-                    chart_y_start = 75
+                    chart_y_start = 60
                     chart_x_spacing = 90
                     chart_y_spacing = 60
 
                     for i, fig in enumerate(charts_for_listing(row)):  # You'd define this to generate or return 4 Plotly charts
-                        x = 10 + (i % 2) * chart_x_spacing
+                        x = 108 + (i % 2) * chart_x_spacing
                         y = chart_y_start + (i // 2) * chart_y_spacing
-
-                        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
-                            fig.update_layout(font=dict(color="black"), title_font_size=30)
-                            fig.write_image(tmp.name, format="png")
-                            pdf.image(tmp.name, x=x, y=y, w=80)
+                        add_img(pdf, fig, x=x, y=y, w=80)
 
                 # Save the PDF to a BytesIO object
-                pdf_bytes = pdf.output(dest="S").encode("latin-1")  # fpdf uses latin-1 encoding
-                pdf_output = BytesIO(pdf_bytes)
-                for path in temp_paths:
-                    os.remove(path)
-                if st.download_button(
+                pdf_output = BytesIO(pdf.output(dest="S"))
+
+                # display pdf in streamlit
+                # st.write("### Report Preview")
+                # base64_pdf = base64.b64encode(pdf.output(dest="S")).decode("utf-8")
+                # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="900" type="application/pdf"></iframe>'
+                # st.components.v1.html(pdf_display, height=900, width=720)
+                
+                @st.fragment
+                def download_link(object_to_download):
+                    st.download_button(
                     label="Download Report",
                     data=pdf_output,
                     file_name="report.pdf",
                     mime="application/pdf",
-                ):
+                )
+
+                if download_link(pdf_output):
                     st.success("Report downloaded successfully.")
